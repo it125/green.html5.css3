022c762f0dfab957b8b0e266148a1b28d0bc3ae3
it125 <challenge125@daum.net>
2021-12-08 15:58:58 +0900

    20211208

--- javascript/text.js
+++ javascript/text.js
@@ -1,3 +1,8 @@
 document.write("안녕하세요");
 document.write("ㄱ");
 document.write("ㄴ");
+
+document.write('"안녕하세요"');
+document.write("<br />\"안녕하세요\"");
+document.write("<br />\'안녕하세요\'");
+document.write("<br />'안녕하세요'");
